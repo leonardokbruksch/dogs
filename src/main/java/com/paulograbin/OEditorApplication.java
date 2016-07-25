c86ece9f7ab3d852645cda4c3889ea35ef848e25
Paulo Grabin <plgrabin@gmail.com>
2016-07-25 17:15:18 -0300

    Setting up project infrastructure.

---
+++ src/main/java/com/paulograbin/OEditorApplication.java
@@ -0,0 +1,12 @@
+package com.paulograbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OEditorApplication.class, args);
+	}
+}
