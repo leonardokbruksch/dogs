fadda4d5395cc096dcaeaffc4cfc02d58e013a36
Grabin, Paulo <paulo.grabin@sap.com>
2016-12-03 20:54:13 -0200

    Ignoring tests that make use of the actual mysql db.

--- src/test/java/com/paulograbin/persistence/DatabaseNotesRepositoryTest.java
+++ src/test/java/com/paulograbin/persistence/DatabaseNotesRepositoryTest.java
@@ -2,9 +2,8 @@ package com.paulograbin.persistence;
 
 import com.paulograbin.domain.notes.Note;
 import com.paulograbin.domain.notes.NotesRepository;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,6 +12,7 @@ import javax.inject.Inject;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Ignore
 public class DatabaseNotesRepositoryTest extends RepositoryTest {
 
     @Inject
