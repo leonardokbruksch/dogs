3525a1fad6702268b73cedc600519fbcd3d3c39b
Paulo Grabin <plgrabin@gmail.com>
2017-05-01 23:58:37 -0300

    Nothing really important.
    Adding Kotlin to the project.

--- src/main/java/com/paulograbin/OEditorApplication.java
+++ src/main/java/com/paulograbin/OEditorApplication.java
@@ -1,13 +1,17 @@
 package com.paulograbin;
 
+import com.paulograbin.annotations.GrabinClassAnnotation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.lang.annotation.Annotation;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -16,5 +20,13 @@ public class OEditorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OEditorApplication.class, args);
+	}
+
+	@Bean
+	@ConditionalOnClass(String.class)
+	public String aString() {
+		System.out.println("A string has been returned");
+
+		return "aString";
 	}
 }
