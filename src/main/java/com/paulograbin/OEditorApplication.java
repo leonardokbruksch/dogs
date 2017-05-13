f97a08407fa373b940b01202ab56ba53f18d3d66
Grabin, Paulo <paulo.grabin@sap.com>
2017-05-13 16:10:46 -0300

    Removing unused imports.

--- src/main/java/com/paulograbin/OEditorApplication.java
+++ src/main/java/com/paulograbin/OEditorApplication.java
@@ -1,18 +1,10 @@
 package com.paulograbin;
 
-import com.paulograbin.annotations.GrabinClassAnnotation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
-import java.lang.annotation.Annotation;
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableAutoConfiguration // This annotation tells Spring Boot to “guess” how you will want to configure Spring, based on the jar dependencies that you have added.
